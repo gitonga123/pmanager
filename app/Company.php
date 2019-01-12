d3d17e3e3e07c05c57aaf153b0bf9401bf18746a
Daniel Mutwiri <mutwiridanielsci@gmail.com>
2019-01-12 22:09:44 +0300

    Many to Many, One To Many Relationships :smiley:

--- app/Company.php
+++ app/Company.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Company extends Model
 {
     protected $fillable = ['description', 'name', 'user_id'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
--- app/Task.php
+++ app/Task.php
@@ -7,4 +7,24 @@ use Illuminate\Database\Eloquent\Model;
 class Task extends Model
 {
     protected $fillable = ['name', 'description', 'days', 'hours', 'project_id', 'user_id', 'company_id'];
+
+    public function user()
+    {
+        return $this->belongsToMany(User::class);
+    }
+
+    public function project()
+    {
+        return $this->belongsTo(Project::class);
+    }
+
+    public function company()
+    {
+        return $this->belongsTo('Pmanager\Models\Company');
+    }
+
+    public function users()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
