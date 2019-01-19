f24969d9855216c69f518452787680121f62a15d
Daniel Mutwiri <mutwiridanielsci@gmail.com>
2019-01-19 09:21:23 +0300

    Project Controller :smiley:

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -14,7 +14,15 @@ class ProjectController extends Controller
      */
     public function index()
     {
-        //
+        if (Auth::check() && Auth::user()->id === 3) {
+            $projects = Project::all();
+        } else if (Auth::check()) {
+
+            $projects = Project::where('user_id', Auth::user()->id)->get();
+        } else {
+            return redirect()->route('login')->with("Kasambuyu Oyole");
+        }
+        return view('projects.index', compact('projects'));
     }
 
     /**
@@ -24,7 +32,7 @@ class ProjectController extends Controller
      */
     public function create()
     {
-        //
+        return view('projects.create');
     }
 
     /**
@@ -35,51 +43,84 @@ class ProjectController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $project = false;
+        if (Auth::check()) {
+            $project = Project::create([
+                'name' => $request->input('name'),
+                'description' => $request->input(['description']),
+                'user_id' => Auth::user()->id,
+            ]);
+        }
+
+        if($project) {
+            return redirect()->route('projects.show', ['project' => $project->id])->with('success', 'project Created Successfully');
+        } else {
+            return back()->withInput()->with('errors', ['The project could not be created, Contact your Admin']);
+        }
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
+     * @param  Project  $project
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show(Project $project)
     {
-        //
+//        $project =  Project::where('id', $project->id)->first();
+        $project = Project::find($project->id);
+        return view('projects.show', compact('project'));
     }
 
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  int  $id
+     * @param  Project $project
      * @return \Illuminate\Http\Response
      */
-    public function edit($id)
+    public function edit(Project $project)
     {
-        //
+        $project =  Project::find($project->id);
+        return view('projects.edit', compact('project'));
     }
 
     /**
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
+     * @param  project  $project
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request, Project $project)
     {
-        //
+        //save data
+        $projectUpdate = Project::where('id', $project->id)
+            ->update([
+                'name' => $request->input('name'),
+                'description' => $request->input('description')
+            ]);
+        if ($projectUpdate) {
+            return redirect()->route('projects.show', ['project' => $project->id])
+                ->with('success', 'Project Updated Successfully');
+        }
+        // redirect
+        return back()->withInput()->with('errors', ['Project could not be updated']);
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
+     * @param  project  $project
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(Project $project)
     {
-        //
+        $findProject = Project::find($project->id);
+
+        if ($findProject->delete()) {
+            return redirect()->route('projects.index')->with('success', 'Project Deleted Successfully');
+        }
+
+        return back()->withInput()->with('error', ['Project could not be deleted']);
     }
 }
---
+++ resources/views/projects/show.blade.php
@@ -0,0 +1,63 @@
+
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="col-md-9 col-lg-9 col-sm-9 pull-left">
+            <div class="jumbotron">
+             <h1>{{ $company->name }}</h1>
+             <p class="lead">{{ $company->description }}</p>
+                <a href="{{route('projects.create')}}" class="btn btn-sm btn-danger pull-right">New Project</a>
+         </div>
+
+            <div class="row" style="background: white; margin: 10px;">
+
+            @foreach($company->projects as $project)
+                <div class="col-lg-4 col-sm-4 col-md-4">
+                    <h2>{{ $project->name }}</h2>
+                    <p class="text-danger">Days: {{ $project->days }}</p>
+                    <p> {{ $project->description }}</p>
+                    <p><a class="btn btn-primary" href="/projects/{{ $project->id }}" role="button">View Project</a></p>
+                </div>
+            @endforeach
+        </div>
+        </div>
+
+        <div class="col-sm-3 col-md-3 col-lg-3 pull-right">
+            {{--<div class="sidebar-module sidebar-module-inset">--}}
+                {{--<h4>About</h4>--}}
+                {{--<p>Ffiaia <em> Empanticipate YOurself</em> From the vulters. Crass mattis connection pursuit</p>--}}
+            {{--</div>--}}
+
+            <div class="sidebar-module">
+                <h4>Actions</h4>
+                <ol class="list-unstyled">
+                    <li><a href="{{ route('companies.create') }}">New Company</a></li>
+                    <li><a href="/companies/{{ $company->id }}/edit">Edit</a></li>
+                    <li><a href="#"
+                        onclick="
+                        var result = confirm('Are you sure you wish to delete this project ?');
+                        if (result) {
+                            event.preventDefault();
+                            document.getElementById('delete-form').submit();
+                        }
+                        ">Delete</a></li>
+                    <form id="delete-form" method="POST" style="display: none;" action="{{ route('companies.destroy', [$company->id])}}">
+                        <input type="hidden" name="_method" value="delete" />
+                        {{ csrf_field() }}
+                    </form>
+
+                </ol>
+            </div>
+
+            <div class="sidebar-module">
+                <h4>Links</h4>
+                <ol class="list-unstyled">
+                    <li><a href="{{ route('projects.create') }}">New Project</a></li>
+                    <li><a href="#">New Member</a></li>
+                    <li><a href="/companies">View All Campanies</a></li>
+                </ol>
+            </div>
+        </div>
+    </div>
+@endsection
